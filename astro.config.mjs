624a5b0827d0b7ae9acae8f519a10d34852d201c
Madhva8 <fmadhva@gmail.com>
2026-02-23 06:26:55 +0100

    Add Vercel adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   site: 'https://madhva.eu',
   integrations: [mdx(), sitemap()],
-  adapter: vercel(),
 });
